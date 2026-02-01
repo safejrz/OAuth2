d4183cf88e3a4fa95bb9c4156655e89477ff5d2e
Javier Rivera <ingjrz@gmail.com>
2026-02-01 15:15:21 -0600

    adding basic authorization

--- Program.cs
+++ Program.cs
@@ -1,33 +1,52 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 
+const string AuthScheme = "cookie";
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie");
+builder.Services.AddAuthentication(AuthScheme)
+    .AddCookie(AuthScheme);
 
 var app = builder.Build();
 
 app.UseAuthentication();
 
 //Recognizing the authenticated user from the auth cookie
-app.MapGet("/username", (HttpContext ctx) =>
+app.MapGet("/unsecure", (HttpContext ctx) =>
     {
-        return ctx.User.FindFirst("usr").Value;
+        var userClaim = ctx.User?.FindFirst("usr");
+        return userClaim?.Value ?? "empty";
+    });
+
+app.MapGet("/sweden", (HttpContext ctx) =>
+    {
+        if(!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+        {
+            ctx.Response.StatusCode = 401;
+            return "";
+        }
+        
+        if (!ctx.User.HasClaim("passport_type", "eur"))
+        {
+            ctx.Response.StatusCode = 403;
+            return "";
+        }
+        
+        return "allowed";
     });
 
 //Creating the auth cookie
 app.MapGet("/login", async (HttpContext ctx) =>
     {
-        //ctx.SignIn();
-        var claims = new List<Claim>();
-        claims.Add(new Claim("usr", "javi"));
-        var identity = new ClaimsIdentity(claims, "cookie");
+        var claims = new List<Claim>
+        {
+            new Claim("usr", "javi"),
+            new Claim("passport_type", "eur")
+        };
+        var identity = new ClaimsIdentity(claims, AuthScheme);
         var user = new ClaimsPrincipal(identity);
-        
-        await ctx.SignInAsync("cookie", user);
-        return "ok";
+        await ctx.SignInAsync(AuthScheme, user);
     });
 
 app.Run();
