b474dd718b46853a3a79d0d22d0f09878bcfd40b
Javier Rivera <ingjrz@gmail.com>
2026-02-01 14:28:12 -0600

    Same code but using the MSFT implementation

--- Program.cs
+++ Program.cs
@@ -1,66 +1,35 @@
 ﻿using System.Security.Claims;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDataProtection();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<AuthService>();
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie");
 
 var app = builder.Build();
 
-app.Use((ctx, next) =>
-{
-    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-    var protector = idp.CreateProtector("auth-cookie");
-
-    var authcookie = ctx.Request.Headers.Cookie.FirstOrDefault(c => c.StartsWith("auth="));
-    var protectedPayload = authcookie?.Split('=').Last();
-    var payload = protectedPayload is null ? null : protector.Unprotect(protectedPayload);
-    var parts = payload?.Split(':');
-    var key = parts?.FirstOrDefault();
-    var value = parts?.LastOrDefault();
-
-    var claims = new List<Claim>();
-    claims.Add(new Claim(key, value));
-    var identity = new ClaimsIdentity(claims);
-    ctx.User = new ClaimsPrincipal(identity);
-
-    return next();
-});
+app.UseAuthentication();
 
 //Recognizing the authenticated user from the auth cookie
-app.MapGet("username", (HttpContext ctx) =>
+app.MapGet("/username", (HttpContext ctx) =>
     {
         return ctx.User.FindFirst("usr").Value;
     });
 
 //Creating the auth cookie
-app.MapGet("/login", (AuthService auth) =>
+app.MapGet("/login", async (HttpContext ctx) =>
     {
-        auth.SignIn();
+        //ctx.SignIn();
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "javi"));
+        var identity = new ClaimsIdentity(claims, "cookie");
+        var user = new ClaimsPrincipal(identity);
+        
+        await ctx.SignInAsync("cookie", user);
         return "ok";
     });
 
 app.Run();
-
-
-public class AuthService
-{
-    private readonly IDataProtectionProvider _idp;
-    private readonly IHttpContextAccessor _accessor;
-
-    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
-    {
-        _idp = idp;
-        _accessor = accessor;
-    }
-
-    public void SignIn()
-    {
-        var protector = _idp.CreateProtector("auth-cookie");
-        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:javier")}";
-    }
-}
